2096765da0a39191595ac1b56a4128775e2866ba
SamirArap <samir.arap@gmail.com>
2016-12-24 15:37:25 +0100

    Added validates in model and def create in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'contacts/new'
-
   root to: 'pages#home'
   get 'pages/about'
+
+  resources :contacts, only: :create
+  get 'contact-us', to: "contacts#new", as: 'new_contact'
 
 end
