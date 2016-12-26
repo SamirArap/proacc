ede2caf007eb5527a4d3d9c2da60caff916b021b
SamirArap <samir.arap@gmail.com>
2016-12-26 13:12:25 +0100

    Modified the sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224213347) do
+ActiveRecord::Schema.define(version: 20161224224056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20161224213347) do
     t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string  "name"
+    t.decimal "price"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,6 +41,7 @@ ActiveRecord::Schema.define(version: 20161224213347) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "plan_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
