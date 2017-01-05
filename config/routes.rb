a93e21db537d58d9d737561c436b33113a6fd6a4
SamirArap <samir.arap@gmail.com>
2017-01-05 16:49:03 +0100

    Added profiles controller and user profile form

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  #resources :users
+  resources :users do
+    resource :profile
+  end
 
   resources :contacts, only: :create
   get 'contact-us', to: "contacts#new", as: 'new_contact'
