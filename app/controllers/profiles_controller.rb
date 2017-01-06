a4a625df7fb91ab41d1c16fd79485f955fbbfab3
SamirArap <samir.arap@gmail.com>
2017-01-06 04:24:45 +0100

    Added avatar column to Profile table and file field for image upload

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,7 +8,7 @@ class ProfilesController < ApplicationController
   #POST to /user/:user_id/profiles
   def create
     @user = User.find(params[:user_id])
-    @profile = @user.build_profile(profile_params)
+    @profile = @user.build_profile( profile_params )
 
     if @profile.save
       flash[:success] = "Profile updated!"
@@ -23,13 +23,6 @@ class ProfilesController < ApplicationController
 
   private
   def profile_params
-    params.require(:profile).permit(
-        :first_name,
-        :last_name,
-        :job_title,
-        :phone_number,
-        :contact_email,
-        :description
-    )
+    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description )
   end
 end
